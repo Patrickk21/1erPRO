eaaf414eb0b70302e624cd02ce88c120c686982f
Patrickk <ma17.dabky@gmail.com>
2023-03-29 02:14:36 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://primodbk.onrender.com', {
+    const response = await fetch('https://codex11.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
